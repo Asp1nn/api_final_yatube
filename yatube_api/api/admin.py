1fbc30d4d2dd9bb7a913e78be74d8cd54c2f9556
Павел Стерехов <iiabejic@gmail.com>
2021-07-06 22:46:40 +0500

    New

--- yatube_api/api/admin.py
+++ yatube_api/api/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Group, Comment, Follow
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "author", "group", 'image',)
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
+    empty_value_display = "-пусто-"
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_field = ('title',)
+    list_filter = ('title',)
+    empty_value_display = "-пусто-"
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'text', 'created',)
+    search_fields = ('author',)
+    list_filter = ('created',)
+    empty_value_display = "-пусто-"
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following',)
+    search_fields = ('following',)
+    list_filter = ('following',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from .views import CommentViewSet, PostViewSet, GroupViewSet, FollowViewSet
 
